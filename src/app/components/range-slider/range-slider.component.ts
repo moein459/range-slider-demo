4c72823eb85d73bc742dade73d808c76f5159484
MoeinZ <moein459@gmail.com>
2019-10-31 20:15:52 +0330

    add delay to value container offset

--- src/app/components/range-slider/range-slider.component.ts
+++ src/app/components/range-slider/range-slider.component.ts
@@ -1,8 +1,8 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {debounceTime, map, pairwise} from 'rxjs/operators';
+import {debounceTime, delay, map, pairwise} from 'rxjs/operators';
 import {animate, state, style, transition, trigger} from '@angular/animations';
-import {Subscription} from 'rxjs';
+import {BehaviorSubject, Subscription} from 'rxjs';
 
 @Component({
 	selector: 'app-range-slider',
@@ -28,10 +28,16 @@ export class RangeSliderComponent implements OnInit, OnDestroy {
 	@Input()
 	default = 70;
 
+	@Input()
+	delay = 50;
+
 	formControl = new FormControl([this.default]);
 
 	focused = false;
 	directionState: 'right' | 'left' | 'unset';
+
+	private offset$ = new BehaviorSubject<string>(this.calculateOffset());
+	offset = this.offset$.asObservable().pipe(delay(this.delay));
 
 	sub = new Subscription();
 
@@ -51,15 +57,10 @@ export class RangeSliderComponent implements OnInit, OnDestroy {
 		return `linear-gradient(90deg, #4834D4 ${this.percentageValue}%, #D2E2FF 0)`;
 	}
 
-	get offset() {
-		const offset = Math.floor(34 * (this.percentageValue / 100));
-		return `calc(${this.percentageValue}% - ${offset}px`;
-	}
-
 	ngOnInit() {
 		const sub1 = this.formControl.valueChanges
-			.pipe(debounceTime(50))
-			.subscribe(value => {
+			.pipe(debounceTime(this.delay))
+			.subscribe(() => {
 				this.directionState = 'unset';
 			});
 
@@ -69,10 +70,16 @@ export class RangeSliderComponent implements OnInit, OnDestroy {
 				map(([prev, curr]) => curr > prev ? 'right' : 'left'))
 			.subscribe(value => {
 				this.directionState = value;
+				this.offset$.next(this.calculateOffset());
 			});
 
 		this.sub.add(sub1);
 		this.sub.add(sub2);
+	}
+
+	calculateOffset() {
+		const offset = Math.floor(34 * (this.percentageValue / 100));
+		return `calc(${this.percentageValue}% - ${offset}px)`;
 	}
 
 	mouseDown() {
